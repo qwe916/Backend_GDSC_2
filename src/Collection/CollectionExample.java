2cb66632d29536c3153b89af77d5f7e9126ef4ce
qwe916 <qwe916@naver.com>
2022-11-02 22:45:39 +0900

    Collection

---
+++ src/Collection/CollectionExample.java
@@ -0,0 +1,19 @@
+package Collection;
+
+import java.util.*;
+
+public class CollectionExample {
+    public static void main(String[] args) {
+        //List
+        List<Integer> myArrayList = new ArrayList<>();
+        List<Integer> myLinkedList = new LinkedList<>();
+        //Set
+        Set<Integer> myHashSet = new HashSet<>();
+        Set<Integer> myTreeSet = new TreeSet<>();
+        //Queue
+        Queue<Integer> myPriorQueue = new PriorityQueue<>();
+        Queue<Integer> myArrayDeque = new ArrayDeque<>();
+        //Map
+        Map<String, Integer> myHashMap = new HashMap<>();
+    }
+}
